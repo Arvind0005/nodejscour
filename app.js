60fabf0d221cf43611ffdb9f32bcb863f8791d5f
Arvind_Suresh <53540697+Arvind0515@users.noreply.github.com>
2021-08-10 00:53:43 +0530

    connecting to mongodb

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const dburi ="mongodb+srv://Arvind:password05@cluster0.rayzm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; 
 const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000,function(req,res)
+mongoose.connect(dburi,{useNewUrlParser:true,useUnifiedTopology:true}).then((result)=>
 {
+    app.listen(3000,function(req,res)
+    {
     console.log("server listening @ port 3000");
-})
+    })
+}).catch((err)=>
+{
+    console.log(err);
+});
 
-app.get('/',function(req,res)
+app.set('view engine','ejs');
+
+// app.listen(3000,function(req,res)
+// {
+//     console.log("server listening @ port 3000");
+// })
+
+app.use(morgan('dev'));
+
+app.get('/',function(req,res,next)
 {
     res.sendFile('./views/index.html',{root:__dirname});
     //res.send("<p>hii hello how are you</p>");
+    next();
 });
+
+app.use(function(req,res,next)
+{
+    console.log("in the next");
+    next();
+})
 app.get('/about',function(req,res)
 {
     res.sendFile('./views/about.html',{root:__dirname});
